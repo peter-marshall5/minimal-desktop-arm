4a9443c4303061f61a33f1defbdd3a529e363f53
Peter Marshall <petms@proton.me>
2024-01-01 17:01:32 -0500

    Remove example image build

--- flake.nix
+++ flake.nix
@@ -3,14 +3,10 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixos-appliance = {
-      url = "github:peter-marshall5/nixos-appliance";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     u-boot.url = "github:peter-marshall5/u-boot-veyron-speedy";
   };
 
-  outputs = { self, nixpkgs, nixos-appliance, u-boot, ... }: {
+  outputs = { self, nixpkgs, u-boot, ... }: {
 
     nixosModules.cross-armv7 = {config, ...}: {
       nixpkgs.config.allowUnsupportedSystem = true;
@@ -19,15 +15,6 @@
     };
 
     nixosModules.veyron-speedy = import ./veyron-speedy.nix;
-
-    images.speedy = nixos-appliance.nixosGenerate {
-      system = "armv7l-linux";
-      modules = [
-        ./configuration.nix
-        self.nixosModules.cross-armv7
-        self.nixosModules.veyron-speedy
-      ];
-    };
 
   };
 }
